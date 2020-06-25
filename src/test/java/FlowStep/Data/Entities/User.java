4efd81dfdd64d11aac282e0bf7053708ba6164fc
Duong Nguyen <66982564+duyduong-nguyen2711@users.noreply.github.com>
2020-06-25 14:15:34 +0700

    Git check rebase

--- src/test/java/FlowStep/Data/Entities/User.java
+++ src/test/java/FlowStep/Data/Entities/User.java
@@ -2,14 +2,4 @@ package FlowStep.Data.Entities;
 
 public class User {
 
-    public String fullName;
-    public String email;
-    public String password;
-
-    public User(String name, String userEmail, String passWord){
-        name =  fullName;
-        userEmail = email;
-        passWord = password;
-    }
-
 }
